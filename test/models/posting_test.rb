d1882879fa14e972f52e34f069f2f4ffe983cde5
Shawn Michaud <shawnmne@hotmail.com>
2016-05-04 14:44:50 -0500

    model tests done

--- test/models/posting_test.rb
+++ test/models/posting_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "posting is valid" do
+  	p = Posting.new(job_title:"Developer", company_name:"IBM", description:"fun", location:"here", how_to_apply:"telepathically")
+    assert(p.valid?, "Should be valid with all attributes present")
+  end
 end
